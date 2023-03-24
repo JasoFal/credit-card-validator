963596f5412679ee146f7e196f076df83868a91f
Jason Falk <jasonfalk9@gmail.com>
2023-03-24 11:07:22 -0700

    Add functionality to collectAndVerifyCCNum where if the cc number is less than 15 the function returns a warning.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Utility Logic
+
+// Business Logic
+function collectAndVerifyCCNum(Number) {
+  if (Number.toString().length < 15) {
+    return "Invalid Credit Card Number";
+  }
+
+} 
+
+collectAndVerifyCCNum(410208086043);
+// UI Logic
