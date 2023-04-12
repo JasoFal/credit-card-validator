722a0dbbc57a7495078e2adb2cdacd52902001d0
Jason Falk <jasonfalk9@gmail.com>
2023-04-12 16:00:50 -0700

    Add functionality to apply Luhn Algorithm to given cc number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,73 @@
 // Utility Logic
-
 // Business Logic
-function VerifyCCType(Number) {
-  const ccNumber = Number.toString().split("");
-  if (ccNumber[0] == "3" && ccNumber[1] === "4" || ccNumber[1] === "7") {
+function verifyCCType(num) {
+  const ccNumberStringArray = num.toString().split("");
+  if (ccNumberStringArray[0] == "3" && ccNumberStringArray[1] === "4" || ccNumberStringArray[1] === "7") {
     return "American Express";
-  } else if (ccNumber[0] == "4") {
+  } else if (ccNumberStringArray[0] == "4") {
     return "VISA"
-  } else if (ccNumber[0] == "5") {
+  } else if (ccNumberStringArray[0] == "5") {
     return "Mastercard"
-  } else if (ccNumber[0] == "6") {
+  } else if (ccNumberStringArray[0] == "6") {
     return "Discover"
   } else {
-    return "Invalid Credit Card"
+    return false;
   }
 }
 
-function collectAndVerifyCCNum(Number) {
-  if (Number.toString().length === 15 || Number.toString().length === 16) {
-    const startingNumber = Number.toString().split("");
-    const ccTestNumber = [];
-    startingNumber.forEach(ele => ccTestNumber.push(+ele));
-    ccTestNumber.reverse();
-    return ccTestNumber;
+function verifyCCNum(num) {
+  if (num.toString().length === 15 || num.toString().length === 16) {
+    return true;
   } else {
-    return "Invalid Credit Card";
+    return false;
   }
 }
 
-function applyLuhnAlgorithm() {
-  collectAndVerifyCCNum[1,3,5,7] * 2;
-  // let luhnTestedWord = collectAndVerifyCCNum().map(function(Element) {
-  //   return Element * 2;
-  // });
-  // console.log(luhnTestedWord);
+function doubleNumSum(num) {
+  let doubleNum = num * 2;
+  if (Math.ceil(Math.log10(doubleNum + 1)) > 1) {
+    return sumOfDigits(doubleNum);
+  } else {
+    return doubleNum;
+  }
 }
-applyLuhnAlgorithm;
 
+function sumOfDigits(num) {
+  let remainder, sum = 0;
+  while(num){
+      remainder = num % 10;
+      sum = sum + remainder;
+      num = Math.floor(num/10);
+  }
+  return sum;
+}
 
-console.log(applyLuhnAlgorithm());
+function luhnAlgorithm(num) {
+  const startingNumber = num.toString().split("");
+  let ccTestNumber = startingNumber.reverse().map(function (item) {
+    return parseInt(item);
+  });
+  let isValidCCType = verifyCCType;
+  let isValidCCNum = verifyCCNum;
+  let doubledNumArray = [];
+  if (!isValidCCType || !isValidCCNum) {
+    return "Invalid Credit Card Number";
+  } else {
+    ccTestNumber.forEach(function (num, index) {
+      if (index % 2 == 0) {
+        doubledNumArray.push(doubleNumSum(num));
+        console.log(doubledNumArray, "doubledNumArray2ElectricBoogaloo");
+      } else {
+        doubledNumArray.push(num);
+        console.log(doubledNumArray, "doubledNumArray1");
+      }
+    });
+    console.log(doubledNumArray, "doubledNumArray");
+    // console.log(tempStringArray, "tempStringArray");
+  }
+  let doubledArraySum = doubledNumArray.reduce((a,b) => a+b, 0);
+  console.log(doubledArraySum, "doubledArraySum");
+}
+
+luhnAlgorithm(4102080860435620);
 // UI Logic
